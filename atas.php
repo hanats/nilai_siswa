94eece2aff5d6631515f12fb606fbdddd71bcde6
hanats <92453333+hanats@users.noreply.github.com>
2022-03-16 22:43:07 +0700

    Add files via upload

---
+++ atas.php
@@ -0,0 +1,13 @@
+<header class="container" role="banner">
+    <div class="row justify-content-between">
+        <div class="col">
+            <a class="navbar-brand ps-3" href="index.html">Student Activity Score - STTNF</a>
+            <!-- <h1>Student Activity Score - STTNF</h1> -->
+        </div>
+        <div class="col">
+            <a href="#">Home</a> | <a href="#">Activity</a> | <a href="#">My Score</a>
+            <a href="#">Login</a>
+        </div>
+    </div>
+</header>
+    
---
+++ bawah.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SAS-STTNF</title>
+</head>
+</body>
+    <footer class="p-3 mb-2 bg-dark text-white">
+        <p>Created by <a href="http://nurulfikri.ac.id">Pusinfo NF &copy;2017</a></p>
+    </footer>
+</body>
+</html>
